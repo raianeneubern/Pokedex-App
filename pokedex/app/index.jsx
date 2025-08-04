93047e31696458624d88a2d4b5031947eeed9c43
raianeneubern <raianeneubern@gmail.com>
2025-08-04 08:03:56 -0300

    slide 44

---
+++ pokedex/app/index.jsx
@@ -0,0 +1,36 @@
+import {  FlatList, StyleSheet, Text, View } from "react-native";
+import Header from '@/components/Header';
+import Card from "../components/Card";
+import { pokemons } from "@/data/pokemons"
+
+export default function Index() {
+
+  const renderPokemon = ({ item }) => (
+    <Card pokemon={item}/>
+  )
+  return (
+    <View style={styles.container}>
+      <Header title="Pokédex" />
+      <FlatList 
+      data={pokemons}
+      style={styles.cards}
+      keyExtractor={(pokemon) => pokemon.Numero.toString()}
+      renderItem={ renderPokemon }
+      initialNumtoRender={10}
+      maxToRenderPerBatch={10}
+      windowSize={5}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: "column",
+ },
+  cards: {
+    padding: 15,
+  },
+});
